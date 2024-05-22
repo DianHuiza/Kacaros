b3726fad99b3047aea0fa71cfc884dfc77ec1133
DianHuiza <dianhuiza@gmail.com>
2024-05-22 13:40:27 -0400

    create useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,1 +1,16 @@
-export function useDebounce (callback, timeout = 300) {}
+import { useCallback, useRef } from 'react'
+
+export function useDebounce<T extends any[]> (debounce: (...args: T) => void, timeout = 300) {
+  const timerRef = useRef<number | null>(null)
+  
+  const debouncedFn = useCallback((...args: T) => {
+    if (timerRef.current != null) {
+      clearTimeout(timerRef.current)
+    }
+    timerRef.current = setTimeout(() => {
+      debounce(...args)
+    }, timeout)
+  }, [debounce, timeout])
+
+  return { debouncedFn }
+}
